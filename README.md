c209ea50a3f87427cf8b0303d4826a9f2d85d9a3
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-05-01 13:56:13 +0530

    Moved server to gulp default

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ It's a simple TODO list made using flux architecure, ES6 and ReactJS. [Live Demo
 ## Usage
 - Clone the repo
 - Run `npm install`
-- Run `node server.js` for a local demo on `http://localhost:8000`
 - Run `gulp` for automatic build, watch and livereload.
+- A server will also be started @ `http://localhost:8000` for live demo.
 
 ## Author
 Vivek Kumar Bansal
--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,9 @@ var gulp    = require('gulp'),
     uglify = require('gulp-uglify'),
     streamify = require('gulp-streamify'),
     rename = require('gulp-rename'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    Express = require("express"),
+    app = new Express();
 
 var handleErrors = function(){
     var args = Array.prototype.slice.call(arguments);
@@ -61,10 +63,16 @@ gulp.task('lint',function(){
 
 gulp.task('default', function(){
     lr.listen();
+    
     browserifyTask({
         development: true,
         src: './src/app.js',
         dest: './dist/',
         name: 'app.js'
     });
+    
+    app.use(Express.static(__dirname));
+    app.listen(8000, 'localhost');
+    console.log("Server started at http://localhost:8000");
+    
 });
